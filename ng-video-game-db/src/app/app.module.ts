d605fc8b207135d0c4a0a2cdc3cdbbbd8c267bb9
S-Najim-S <saidnajimullahs@yahoo.com>
2022-03-14 19:40:04 +0100

    details page completed

--- ng-video-game-db/src/app/app.module.ts
+++ ng-video-game-db/src/app/app.module.ts
@@ -17,9 +17,10 @@ import { HomeComponent } from './components/home/home.component';
 import { HttpHeaderInterceptor } from './interceptors/http-headers.interceptor';
 import { HttpErrorsInterceptor } from './interceptors/http-errors.interceptor';
 import { DetailsComponent } from './components/details/details.component';
+import { GameTabsComponent } from './components/game-tabs/game-tabs.component';
 
 @NgModule({
-  declarations: [AppComponent, SearchBarComponent, HomeComponent, DetailsComponent],
+  declarations: [AppComponent, SearchBarComponent, HomeComponent, DetailsComponent, GameTabsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ ng-video-game-db/src/app/components/game-tabs/game-tabs.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Game } from 'src/app/models';
+
+@Component({
+  selector: 'app-game-tabs',
+  templateUrl: './game-tabs.component.html',
+  styleUrls: ['./game-tabs.component.scss'],
+})
+export class GameTabsComponent implements OnInit {
+  constructor() {}
+
+  @Input() game!: Game;
+  ngOnInit(): void {}
+}
